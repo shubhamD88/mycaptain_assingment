3a52aa6d5df631175ef3c995878205724e555323
shubhamD88 <94123771+shubhamD88@users.noreply.github.com>
2022-01-09 17:00:27 +0530

    Add files via upload

---
+++ Area_and_Extention.py.py
@@ -0,0 +1,12 @@
+r= input ("Enter Radius of Circle : ")
+float(r)
+area = float(3.14) *(float(r)*float(r))
+float(area)
+print("The area of circle with radius " +r+" is")
+print(area)
+s=input("Enter the string : ")
+x=s.split(".")
+if x[1]=="py":
+    print("The extention is python")
+    
+
